9537998a975b014474d6e372ada953f0c5a136e9
熊明静 <mingjinger@gmail.com>
2020-11-17 16:45:12 +0800

    init springboot project

---
+++ src/main/java/com/bearomance/helloworld/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.bearomance.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/test")
+@RestController
+public class TestController {
+
+    @GetMapping("/t1")
+    public String t1() {
+        return "Hello Bear";
+    }
+}
